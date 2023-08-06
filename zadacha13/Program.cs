6ce50da67c978ab6a48aa6a3f16119894de6da66
L1GH7n1nG <l1ghtawp1@gmail.com>
2023-08-06 06:07:00 +0300

    Added finished zadacha13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Third(int num)
+{
+	if (num > 0)
+	{
+		while (num > 999) // кол-во "9" номер искомой цифры. 9999 - 4 искомая цифра.
+		{
+			num = num / 10;
+		}
+		int result = num % 10;
+		return result;
+	}
+	else
+	{
+		while (num < -999)
+		{
+			num = num / 10;
+		}
+		int result = (num % 10) * -1;
+		return result;
+	}
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 || number <= -100)
+{
+	int result_main = Third(number);
+	Console.WriteLine($"Третья цифра числа {number} -> {result_main}");
+}
+else Console.Write("третьей цифры нет");
