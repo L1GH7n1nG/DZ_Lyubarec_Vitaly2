d6ec9d216bc496e097f0ae0ae0ccc2595a4e3b64
L1GH7n1nG <l1ghtawp1@gmail.com>
2023-08-06 06:06:50 +0300

    Added finished zadacha10

---
+++ zadacha10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Second(int num)
+{
+	if (num > 0)
+		return num % 100 / 10;
+	else return (num % 100 / 10) * -1;
+}
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number < 1000 || number <= -100 && number > -1000)
+{
+	int result = Second(number);
+	Console.WriteLine($"Вторая цифра числа {number} -> {result}");
+}
+else Console.WriteLine("Неверное число");
