b504d27526fb53e9889ab052e0560edb5ba4fff2
L1GH7n1nG <l1ghtawp1@gmail.com>
2023-08-06 06:07:06 +0300

    Added finished zadacha15

---
+++ zadacha15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+bool day(int num)
+{
+	return num > 5;
+}
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 && number <= 7)
+{
+	bool result = day(number);
+	Console.WriteLine(result ? "Да" : "Нет");
+}
+else Console.Write("Неверно обозначен день недели");
